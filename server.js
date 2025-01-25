1269a97c43c32d70030375ecb1e0865ccd917986
Pranav Raghuvanshi <pranavraghuvanshi2924@gmail.com>
2025-01-25 15:49:58 +0530

    mongoose connected

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
 const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/urlShortner", );
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +12,10 @@ app.set("views", "views")
 
 app.get("/", (req, res)=>{
     res.render("index.ejs");
+})
+
+app.post("/shortUrls", (req, res)=>{
+
 })
 
 app.listen(port, ()=>{
