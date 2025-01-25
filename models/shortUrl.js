2fc94e7d578b7820a46314eec080e9b454ccd1d5
Pranav Raghuvanshi <pranavraghuvanshi2924@gmail.com>
2025-01-25 16:20:48 +0530

    url shortner

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -11,7 +11,7 @@ const shortUrlSchema = new mongoose.Schema({
         required: true,
         default: shortId.generate
     },
-    click:{
+    clicks:{
         type: Number,
         required: true,
         default: 0
--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const ShortUrl = require("./models/shortUrl");
 
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/urlShortner", );
 
 app.set('view engine', 'ejs');
-
 app.set("views", "views")
+app.use(express.urlencoded({extended:false}))
 
-app.get("/", (req, res)=>{
-    res.render("index.ejs");
+app.get("/", async (req, res)=>{
+    const shortUrls = await ShortUrl.find()
+    res.render("index.ejs", {shortUrls: shortUrls});
 })
 
-app.post("/shortUrls", (req, res)=>{
+app.post("/shortUrls", async (req, res)=>{
+    await ShortUrl.create({
+        full: req.body.fullUrl
+    })
+    res.redirect("/");
+})
 
+app.get("/:shortUrl", async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl});
+    if(shortUrl==null) return res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
 })
 
 app.listen(port, ()=>{
